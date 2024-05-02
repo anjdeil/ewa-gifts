71212dffc6c553b2495f5388a87aeb900add33f5
Yevhen <camelcaseman@gmail.com>
2024-05-02 10:18:59 +0200

    Refactored fetchBooks()

--- src/components/TestToolkit/TestToolkit.tsx
+++ src/components/TestToolkit/TestToolkit.tsx
@@ -1,11 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import WooRestApi from "@/services/WooRestApi";
-import {
-    productsFetching,
-    productsFetchingSuccess,
-    productsFetchingError
-} from "@/store/reducers/productListSlice";
+import { fetchProducts } from "@/store/reducers/ActionCreators";
 
 const TestToolkit = () => {
     const wooRestApi = new WooRestApi();
@@ -13,14 +9,7 @@ const TestToolkit = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(productsFetching());
-        wooRestApi.getProducts({ per_page: 24 })
-            .then((data) => {
-                dispatch(productsFetchingSuccess(data))
-            })
-            .catch((err) => {
-                dispatch(productsFetchingError(err.message))
-            })
+        dispatch(fetchProducts(wooRestApi));
     }, [])
 
     const { products, isLoading, error } = useSelector(state => state.productList);
@@ -38,9 +27,9 @@ const TestToolkit = () => {
             <h2>Products</h2>
             <ul>
                 {
-                    products.map(({ name, price }) => {
+                    products.map(({ id, name, price }) => {
                         return (
-                            <li>{name}, <b>${price}</b></li>
+                            <li key={id}>{name}, <b>${price}</b></li>
                         )
                     })
                 }
--- src/store/reducers/ActionCreators.ts
+++ src/store/reducers/ActionCreators.ts
@@ -1,18 +1,32 @@
 import axios from "axios";
 import { IUser } from "@/modules/IUser";
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import productListSlice, {
+    productsFetching,
+    productsFetchingSuccess,
+    productsFetchingError
+} from "./productListSlice";
+
 
 export const fetchUsers = createAsyncThunk(
     'user/fetchAll',
-    async (_, thunkAPI) =>
-    {
-        try
-        {
+    async (_, thunkAPI) => {
+        try {
             const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users');
             return response.data;
-        } catch (error)
-        {
+        } catch (error) {
             return thunkAPI.rejectWithValue("Can't get the users :(")
         }
     }
 );
+
+export const fetchProducts = (wooRestApi) => (dispatch) => {
+    dispatch(productsFetching());
+    wooRestApi.getProducts({ per_page: 24 })
+        .then((data) => {
+            dispatch(productsFetchingSuccess(data))
+        })
+        .catch((err) => {
+            dispatch(productsFetchingError(err.message))
+        })
+}
