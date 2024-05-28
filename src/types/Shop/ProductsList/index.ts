ed7d95658c8e3d220011ec4d807404ef2ce2d820
Ilya-Zab <anno.201543@icloud.com>
2024-05-28 11:59:50 +0200

    Changed ProductCardList

--- src/types/Shop/ProductsList/index.ts
+++ src/types/Shop/ProductsList/index.ts
@@ -1,10 +1,17 @@
 import { z } from "zod";
 import { ProductCardSchema } from "../ProductCard";
 
+export const ProductCardAdaptiveColumns = z.object({
+    mobile: z.number().optional(),
+    tablet: z.number().optional(),
+    desktop: z.number().optional(),
+})
+
 export const ProductCardListPropsSchema = z.object({
     isLoading: z.boolean(),
     isError: z.boolean(),
     products: z.array(ProductCardSchema),
+    columns: ProductCardAdaptiveColumns.optional(),
 });
 
 export type ProductCardListProps = z.infer<typeof ProductCardListPropsSchema>;
