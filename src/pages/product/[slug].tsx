5f440fba3ece62d0aaf56d1d44b920f9ff213ef2
VladyslavDykyi <vladislavdykyi33@gmail.com>
2024-06-06 17:33:52 +0300

    Product Info

--- src/pages/product/[slug].tsx
+++ src/pages/product/[slug].tsx
@@ -1,24 +1,37 @@
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 import Head from "next/head";
 import React from "react";
+import styles from './styles.module.scss';
 import { useFetchProductListQuery } from "@/store/wooCommerce/wooCommerceApi";
+import ProductInfo from '@/components/Shop/Product/ProductInfo';
+
+import Breadcrumbs from "@/components/Layouts/Breadcrumbs";
 import {transformProductCard} from "@/services/transformers";
+import transformBreadcrumbsCategories from "@/services/transformers/woocommerce/transformBreadcrumbsCategories";
+// import wooCommerceRestApi from "@/services/wooCommerce/wooCommerceRestApi";
 
 
 const Product = () =>
 {
 
+  // const router = useRouter();
+  // const { slug } = router.query;
 
-  const router = useRouter();
-  const { slug } = router.query;
-
-  const { data } = useFetchProductListQuery({ slug });
-  let products;
+  const { data } = useFetchProductListQuery({slug: 'dlugopis-nash-z-bialym-korpusem-i-kolorwym-uchwytem' });
+  console.log(data,'useFetchProductListQuery');
+  let info;
   if (data) {
-      products = transformProductCard(data);
+    info = transformProductCard(data);
   }
-  const {name,images,description,sku} = products.at(0);
-  console.log(name,images,description,sku);
+
+  if (!info || info.length === 0) {
+    return <p>No product found</p>;
+  }
+
+
+  const [{ name, categories }] = info;
+
+  const links = transformBreadcrumbsCategories(categories);
 
   return (
     <>
@@ -28,13 +41,11 @@ const Product = () =>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <div>
-
-          <p>
-            {slug}
-          </p>
+      <main className={styles.product__info}>
+        <div className="container">
+          <Breadcrumbs links={links} />
           <div>
+              <ProductInfo data={info}/>
           </div>
         </div>
       </main>
@@ -42,4 +53,34 @@ const Product = () =>
   );
 }
 
+
+// export const getServerSideProps = async ({ query }) => {
+//     const { slugs } = query;
+//     if (!slugs || !Array.isArray(slugs)) {
+//         return {
+//             notFound: true,
+//         };
+//     }
+//     const pageSlugIndex = slugs.findIndex(slug => slug === 'page');
+//     const lastCategorySlugIndex = pageSlugIndex >= 0 ? pageSlugIndex : slugs.length;
+//     const categorySlugs = slugs.slice(0, lastCategorySlugIndex);
+//
+//     const categoryPromises = categorySlugs.map((slug) => wooCommerceRestApi.get(`products/categories`, { slug }));
+//     const categoryResponses = await Promise.all(categoryPromises);
+//
+//     const categories = categoryResponses.reduce((prev, curr) => [...prev, curr.data[0]], []);
+//
+//     const notFoundIndex = categories.findIndex(category => category === undefined);
+//     if (notFoundIndex >= 0) {
+//         return {
+//             notFound: true
+//         }
+//     }
+//     return {
+//         props: {
+//             categories,
+//         }
+//     };
+//
+// }
 export default Product;
