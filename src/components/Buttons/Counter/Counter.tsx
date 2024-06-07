ea24571a4286574a020c3bd3b5f81e0bf7705a7e
Ilya-Zab <anno.201543@icloud.com>
2024-06-07 11:08:06 +0200

    Fix create-tabel

--- src/components/Buttons/Counter/Counter.tsx
+++ src/components/Buttons/Counter/Counter.tsx
@@ -1,8 +1,7 @@
-import React, { FC, useEffect, useRef, useState } from "react";
+import React, { FC, useCallback, useEffect, useRef, useState } from "react";
 import styles from './styles.module.scss';
 import { CounterProps } from "@/types";
-import { CircularProgress, Input, Skeleton } from "@mui/material";
-
+import { IconButton, Input } from "@mui/material";
 export const Counter: FC<CounterProps> = ({ count, changeQuantity, isLoading }) =>
 {
     const [localCount, setLocalCount] = useState(count);
@@ -11,84 +10,70 @@ export const Counter: FC<CounterProps> = ({ count, changeQuantity, isLoading }) 
     useEffect(() =>
     {
         setLocalCount(count);
-    }, [count]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     useEffect(() =>
     {
-        if (timerRef.current)
+        if (!isLoading)
         {
-            clearTimeout(timerRef.current);
+            if (timerRef.current)
+            {
+                clearTimeout(timerRef.current);
+            }
+
+            timerRef.current = setTimeout(() =>
+            {
+                changeQuantity(localCount);
+            }, 1000);
+
+            return () =>
+            {
+                if (timerRef.current)
+                {
+                    clearTimeout(timerRef.current);
+                }
+            };
         }
+    }, [localCount, isLoading, changeQuantity]);
 
-        timerRef.current = setTimeout(() =>
-        {
-            changeQuantity(localCount);
-        }, 1000);
-
-        changeQuantity(localCount);
-
-        // return () =>
-        // {
-        //     if (timerRef.current)
-        //     {
-        //         clearTimeout(timerRef.current);
-        //     }
-        // };
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [localCount]);
-
-    const increment = () =>
+    const increment = useCallback(() =>
     {
         setLocalCount(prevCount => prevCount + 1);
-    };
+    }, []);
 
-    const decrement = () =>
+    const decrement = useCallback(() =>
     {
         setLocalCount(prevCount => (prevCount > 0 ? prevCount - 1 : 0));
-    };
+    }, []);
 
-    const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    const onInputChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) =>
     {
         const newValue = parseInt(event.target.value, 10);
         if (!isNaN(newValue))
         {
             setLocalCount(newValue);
         }
-    }
+    }, []);
 
     return (
         <div className={styles.counter}>
-            <button
-                className={styles.counter__buttons}
-                onClick={decrement}>
+            <IconButton size="large" aria-label="delete" onClick={decrement} >
                 <svg width="16" height="2" viewBox="0 0 16 2" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M1 1H15" stroke="black" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
-            </button>
-            {isLoading
-                ?
-                <Skeleton
-                    height={'35px'}
-                    animation="wave"
-                    className={styles.counter__window}
-                />
-                //     {localCount}
-                // </Skeleton>
-                :
-                <Input
-                    value={localCount}
-                    onChange={onInputChange}
-                    className={styles.counter__window}
-                    type="number"
-                />
-            }
-            <button
-                className={styles.counter__buttons}
-                onClick={increment}>
+            </IconButton>
+            <Input
+                value={localCount}
+                onChange={onInputChange}
+                className={styles.counter__window}
+                type="number"
+            />
+            <IconButton size="large" aria-label="delete" onClick={increment} >
                 <svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M8 0.833496V17.1668M1 9.00016H15" stroke="black" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
-            </button>
+            </IconButton>
         </div>
     );
 };
--- src/components/Shop/CartTable/CartTable.tsx
+++ src/components/Shop/CartTable/CartTable.tsx
@@ -4,21 +4,16 @@ import { Box, Skeleton, Typography } from '@mui/material';
 import styles from './styles.module.scss';
 import { Counter } from '@/components/Buttons';
 import { useAppDispatch } from '@/hooks/redux';
-import { updatedCartQuantity } from '@/store/reducers/CartSlice';
-import { z } from 'zod';
+import { deletedFromCart, updatedCartQuantity } from '@/store/reducers/CartSlice';
+import { CartTableProps, cartProduct } from '@/types/Cart';
+import IconButton from '@mui/material/IconButton';
 
-export const CartTablePropsSchema = z.object({
-    products: z.any(),
-    isLoading: z.boolean()
-});
-
-export type CartTableProps = z.infer<typeof CartTablePropsSchema>
 
 export const CartTable: React.FC<CartTableProps> = ({ products, isLoading }) =>
 {
     const dispatch = useAppDispatch();
 
-    const changeProductsAmount = React.useCallback((product, count) =>
+    const changeProductsAmount = React.useCallback((product: cartProduct, count: string) =>
     {
         dispatch(updatedCartQuantity({
             id: product.id,
@@ -27,14 +22,13 @@ export const CartTable: React.FC<CartTableProps> = ({ products, isLoading }) =>
         }));
     }, [dispatch]);
 
-    console.log(products);
-
-    // const deleteProduct = () => {
-    //     dispatch(deletedFromCart({
-    //         id: ,
-    //         type: ,
-    //     }));
-    // }
+    const deleteProduct = (product: cartProduct) =>
+    {
+        dispatch(deletedFromCart({
+            id: product.id,
+            type: product.type,
+        }));
+    }
 
     return (
         <Box className={styles.CartTable}>
@@ -57,11 +51,11 @@ export const CartTable: React.FC<CartTableProps> = ({ products, isLoading }) =>
                     <Box key={index} className={`${styles.CartTable__row}`}>
                         <Box className={`${styles.cartItem}`}>
                             <Box>
-                                <button className='btn'>
+                                <IconButton aria-label="delete" onClick={() => deleteProduct(product)}>
                                     <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M13 1L1 13M1 1L13 13" stroke="black" strokeLinecap="round" strokeLinejoin="round" />
                                     </svg>
-                                </button>
+                                </IconButton>
                             </Box>
                             <Box>
                                 <Image
