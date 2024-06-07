bcee9f89b40aac4f7436872987db758e96d96455
VladyslavDykyi <vladislavdykyi33@gmail.com>
2024-06-07 18:09:43 +0300

    Page Article v3

--- src/components/Blog/BlogPost/BlogPost.tsx
+++ src/components/Blog/BlogPost/BlogPost.tsx
@@ -1,14 +1,38 @@
-import { FC } from 'react';
-import { BlogItemProps } from '@/types/Blog';
 import styles from './styles.module.scss';
 import Image from "next/image";
-import {Container} from "@mui/material";
+import {Container,Box} from "@mui/material";
+import { styled } from '@mui/material/styles';
 
-interface BlogItemProps {
-    post: BlogItemProps;
-}
+const CustomBox = styled(Box)`
+  .wp-block-image{
+      margin-bottom: 39px;
+  }
+  .wp-block-image img {
+      width: 100%;
+      height: auto;
+      border-radius: 10px;
+      display: block;
+  }
+  .wp-element-caption {
+      margin-top: 12px;
+      font-weight: 400;
+      font-size: 16px;
+      line-height: 150%;
+      color: #696969;
+  }
+  .wp-block-group {
+    background-color: #F6F8FC;
+    border-radius: 10px;
+    padding: 32px;
+    margin-bottom: 22px;
+    & p {
+      margin-bottom: 0;
+    }
+  }
+`;
 
-export const BlogPost: FC<BlogItemProps> = ({ post }) => {
+export const BlogPost = ({ post }) => {
+
     const {date, title,image_src,content } = post;
     const dateTime: Date = new Date(date);
     const monthsPolish: string[] = [
@@ -22,18 +46,18 @@ export const BlogPost: FC<BlogItemProps> = ({ post }) => {
     return (
         <Container  className={styles.article}>
             <header className={styles.article__header}>
+                <h1 className='sub-title'>
+                    {title.rendered}
+                </h1>
                 <time>
                     {`${day} ${month}, ${year}`}
                 </time>
-                <h1 className={`sub-title ${styles.article__title}`}>
-                    {title.rendered}
-                </h1>
             </header>
             <div className={styles.article__img}>
-                <Image src={image_src} alt={title.rendered} sx={{position:'static'}} fill priority/>
+                <Image src={image_src} alt={title.rendered} sx={{position:'static'}} width={1135} height={518} priority/>
             </div>
            <div className={styles.article__text_wrapper}>
-               <div dangerouslySetInnerHTML={{__html: content.rendered}}  className={styles.article__text}/>
+               <CustomBox dangerouslySetInnerHTML={{__html: content.rendered}}  className={styles.article__text}/>
            </div>
         </Container>
     );
