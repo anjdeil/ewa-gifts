391c383ccd2e55c4be0dfe04cfc27fdf1f6efbda
Ilya-Zab <anno.201543@icloud.com>
2024-06-20 10:13:18 +0200

    Added new function to get all color and sizes attributes

--- src/components/Shop/ColorOptions/ColorOptions.tsx
+++ src/components/Shop/ColorOptions/ColorOptions.tsx
@@ -1,17 +1,34 @@
 import { Radio } from '@mui/material';
 import { EwaColorPickIcon, EwaColorPickCheckedIcon } from '@/components/EwaComponents/EwaColorPickIcons';
+import { FC, useState } from 'react';
+import { ColorOptionsProps } from '@/types';
 
-export const ColorOptions = () =>
+
+export const ColorOptions: FC<ColorOptionsProps> = ({ colorAttributes }) =>
 {
-    // return colors?.map(color => (
-    //     <Radio
-    //         key={color.id}
-    //         onChange={handleChange}
-    //         checked={selectedColor === String(color.id)}
-    //         inputProps={{ 'aria-label': color.label }}
-    //         value={color.id}
-    //         icon={<EwaColorPickIcon color={color.cssColor} />}
-    //         checkedIcon={< EwaColorPickCheckedIcon color={color.cssColor} />}
-    //     />
-    // ));
+    // console.log(colorAttributes);
+
+    const [inputId, setInputId] = useState<string | null>(null);
+
+    const onInputClick = (id: string | null) =>
+    {
+        setInputId(id);
+    };
+
+    return colorAttributes.map((color, index) =>
+    {
+        const uniqueId = `color-radio-${index}`;
+
+        return (
+            <Radio
+                key={uniqueId}
+                onChange={() => onInputClick(uniqueId)}
+                checked={uniqueId === inputId}
+                inputProps={{ 'aria-label': color.label }}
+                value={color.name}
+                icon={<EwaColorPickIcon color={color.cssColor} />}
+                checkedIcon={< EwaColorPickCheckedIcon color={color.cssColor} />}
+            />
+        )
+    });
 }
