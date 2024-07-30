14e7d543c84e528adb0bc4569605035dc5da51b0
Ilya-Zab <anno.201543@icloud.com>
2024-07-30 17:42:32 +0200

    Test

--- src/pages/api/custom/[...path].ts
+++ src/pages/api/custom/[...path].ts
@@ -1,21 +1,24 @@
 import { customRestApi } from "@/services/CustomRestApi";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse)
+{
 
     const { path, ...params } = req.query;
 
-    if (!path?.length) {
+    if (!path?.length)
+    {
         return res.status(400).json({ error: 'Failed to fetch, because slug is missing!' });
     }
 
     const slug = typeof path === 'string' ? path : path.join('/');
-
-    try {
+    try
+    {
         const response = await customRestApi.get(slug, params);
 
         res.status(200).json(response.data);
-    } catch (error) {
+    } catch (error)
+    {
         res.status(500);
     }
 }
