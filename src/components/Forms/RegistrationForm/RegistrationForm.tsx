d9d997ea143ef680c92a7d494b24d3c3dd0c162f
Ilya-Zab <168741673+IliaDigiWay@users.noreply.github.com>
2024-08-01 20:18:42 +0200

    Check issue

--- src/components/Forms/RegistrationForm/RegistrationForm.tsx
+++ src/components/Forms/RegistrationForm/RegistrationForm.tsx
@@ -1,16 +1,16 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { forwardRef, useEffect, useImperativeHandle, useState } from "react";
 import { useForm } from "react-hook-form";
-// import { CustomInput } from "../CustomInput";
-// import { Box } from "@mui/material";
+import { CustomInput } from "../CustomInput";
+import { Box } from "@mui/material";
 import { useFetchUserRegistrationMutation } from "@/store/wooCommerce/wooCommerceApi";
 import { useFetchUserTokenMutation } from "@/store/jwt/jwtApi";
 import { useCookies } from 'react-cookie';
 import React from 'react';
-// import variables from '@/styles/variables.module.scss';
-import { CartItem, RegistrationFormSchema } from "@/types";
+import variables from '@/styles/variables.module.scss';
+import { CartItem, RegistrationFormSchema, WpWooError } from "@/types";
 import { z } from "zod";
-// import styles from './styles.module.scss';
+import styles from './styles.module.scss';
 import { registrationUserDataType, userFieldsType } from "@/types/Pages/checkout";
 import { useCreateOrderWoo } from "@/hooks/useCreateOrderWoo";
 import { ShippingLine } from "@/store/reducers/CartSlice";
@@ -38,17 +38,17 @@ export const RegistrationForm = forwardRef<FormHandle, RegistrationFormProps>(({
     const router = useRouter();
     const [cookie, setCookie] = useCookies(['userToken']);
     const [isLoggedIn, setLoggedIn] = useState<boolean>(false);
-    const [isShipping, setShipping] = useState<boolean>(false);
+    // const [isShipping, setShipping] = useState<boolean>(false);
     const formSchema = RegistrationFormSchema(isLoggedIn, isCheckout, isShipping);
     type RegistrationFormType = z.infer<typeof formSchema>;
 
-    const { handleSubmit } = useForm<RegistrationFormType>({
+    const { register, handleSubmit, formState: { errors, isSubmitting, isSubmitSuccessful }, setValue, reset } = useForm<RegistrationFormType>({
         resolver: zodResolver(formSchema)
     });
 
     // function onShippingChange() { setShipping(prev => !prev); }
 
-    const [fetchUserRegistration] = useFetchUserRegistrationMutation();
+    const [fetchUserRegistration, { isError, error }] = useFetchUserRegistrationMutation();
     const [fetchUserToken] = useFetchUserTokenMutation();
     const { createOrder, error: createError, createdOrder } = useCreateOrderWoo();
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -56,7 +56,6 @@ export const RegistrationForm = forwardRef<FormHandle, RegistrationFormProps>(({
 
     useEffect(() =>
     {
-        setShipping(false);
         if (createError)
         {
             alert("Server Error, please try again")
@@ -148,13 +147,13 @@ export const RegistrationForm = forwardRef<FormHandle, RegistrationFormProps>(({
             return { error: (error as Error).message };
         } finally
         {
-            // reset();
+            reset();
         }
     };
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            {/* <Box className={styles.form__wrapper}>
+            <Box className={styles.form__wrapper}>
                 <CustomInput
                     fieldName="Imię"
                     name='name'
@@ -269,7 +268,7 @@ export const RegistrationForm = forwardRef<FormHandle, RegistrationFormProps>(({
                 {isError && <p style={{ color: variables.error }}
                     dangerouslySetInnerHTML={{ __html: (error as WpWooError).data?.message }} />}
             </Box>
-            {isCheckout && <Box className={styles.form__content}>
+            {/* {isCheckout && <Box className={styles.form__content}>
                 <CustomInput
                     fieldName="Wysłać na inny adres?"
                     errors={errors}
