14920ad3d19d08377a54413bf0fb5044edf87ef1
Yevhen <evgen@digiwaymedia.com>
2024-08-01 16:04:09 +0200

    Footer fixed

--- src/pages/api/custom/[...path].ts
+++ src/pages/api/custom/[...path].ts
@@ -1,25 +1,21 @@
 import { customRestApi } from "@/services/CustomRestApi";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse)
-{
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
     const { path, ...params } = req.query;
 
-    if (!path?.length)
-    {
+    if (!path?.length) {
         return res.status(400).json({ error: 'Failed to fetch, because slug is missing!' });
     }
 
     const slug = typeof path === 'string' ? path : path.join('/');
 
-    try
-    {
+    try {
         const response = await customRestApi.get(slug, params);
 
-        res.status(200).json(response.data);
-    } catch (error)
-    {
+        res.status(200).json(response?.data);
+    } catch (error) {
         res.status(500);
     }
 }
