c575962bc1cbbaa8232a0eeb0329d448f52af3e4
Ilya-Zab <168741673+IliaDigiWay@users.noreply.github.com>
2024-08-01 20:29:19 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 import path from 'path';
-const __dirname = new URL( '.', import.meta.url ).pathname;
+const __dirname = new URL('.', import.meta.url).pathname;
 
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join( __dirname, 'styles' )],
+    includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
     domains: ['new.ewagifts.pl', 'ewagifts.pl'],
   },
+  productionBrowserSourceMaps: true,
 };
 
 export default nextConfig;
