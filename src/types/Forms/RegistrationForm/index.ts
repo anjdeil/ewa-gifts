388f033fc95202734663fa6c1c9c4bf229444746
Ilya-Zab <168741673+IliaDigiWay@users.noreply.github.com>
2024-08-02 15:16:30 +0200

    test one theory

--- src/types/Forms/RegistrationForm/index.ts
+++ src/types/Forms/RegistrationForm/index.ts
@@ -1,29 +1,29 @@
 import { z } from "zod";
 
-const passwordSchema = z.string()
-    .min(8, 'The password must contain at least 8 characters')
-    .refine(value => /[A-Z]/.test(value), {
-        message: 'The password must contain at least one capital letter'
-    })
-    .refine(value => /[a-z]/.test(value), {
-        message: 'The password must contain at least one lowercase letter'
-    })
-    .refine(value => /[0-9]/.test(value), {
-        message: 'The password must contain at least one digit'
-    })
-    .refine(value => /[^A-Za-z0-9]/.test(value), {
-        message: 'The password must contain at least one special character'
-    });
+// const passwordSchema = z.string()
+//     .min(8, 'The password must contain at least 8 characters')
+//     .refine(value => /[A-Z]/.test(value), {
+//         message: 'The password must contain at least one capital letter'
+//     })
+//     .refine(value => /[a-z]/.test(value), {
+//         message: 'The password must contain at least one lowercase letter'
+//     })
+//     .refine(value => /[0-9]/.test(value), {
+//         message: 'The password must contain at least one digit'
+//     })
+//     .refine(value => /[^A-Za-z0-9]/.test(value), {
+//         message: 'The password must contain at least one special character'
+//     });
 
-const nipSchema = z.string().min(10, 'Nip number must be 10 characters long')
-    .max(10, 'Nip number must be 10 characters long');
+// const nipSchema = z.string().min(10, 'Nip number must be 10 characters long')
+//     .max(10, 'Nip number must be 10 characters long');
 
-const termsSchema = z.boolean().refine(value => value === true, {
-    message: "You must agree to the terms",
-});
+// const termsSchema = z.boolean().refine(value => value === true, {
+//     message: "You must agree to the terms",
+// });
 
-const phoneSchema = z.string().min(9, 'Phone number must be at least 9 characters long')
-    .max(15, 'Phone number cannot exceed 15 characters');
+// const phoneSchema = z.string().min(9, 'Phone number must be at least 9 characters long')
+//     .max(15, 'Phone number cannot exceed 15 characters');
 
 export const RegistrationFormShippingSchema = z.object({
     name: z.string().min(3, 'Required field'),
@@ -34,11 +34,16 @@ export const RegistrationFormShippingSchema = z.object({
     postCode: z.string().min(5, 'The post code must contain 5 characters'),
     city: z.string().min(1, 'Required field'),
     country: z.string().min(1, 'Required field'),
-    password: passwordSchema,
-    confirmPassword: passwordSchema,
-    phoneNumber: phoneSchema,
-    nip: nipSchema,
-    terms: termsSchema,
+    // password: passwordSchema,
+    password: z.string().min(1, 'Required field'),
+    // confirmPassword: passwordSchema,
+    confirmPassword: z.string().min(1, 'Required field'),
+    // phoneNumber: phoneSchema,
+    phoneNumber: z.string().min(1, 'Required field'),
+    // nip: nipSchema,
+    nip: z.string().min(1, 'Required field'),
+    // terms: termsSchema,
+    terms: z.string().min(1, 'Required field'),
     nameShipping: z.string().min(3, 'Required field'),
     lastNameShipping: z.string().min(3, 'Required field'),
     companyNameShipping: z.string().min(1, 'Required field'),
@@ -46,26 +51,27 @@ export const RegistrationFormShippingSchema = z.object({
     postCodeShipping: z.string().min(5, 'The post code must contain 5 characters'),
     cityShipping: z.string().min(1, 'Required field'),
     countryShipping: z.string().min(1, 'Required field'),
-    phoneNumberShipping: phoneSchema,
+    // phoneNumberShipping: phoneSchema,
+    phoneNumberShipping: z.string().min(1, 'Required field'),
 });
 
-export const RegistrationFormSchema = z.object({
-    name: z.string().min(3, 'Required field'),
-    lastName: z.string().min(3, 'Required field'),
-    email: z.string().email('Please, type valid email'),
-    companyName: z.string().min(1, 'Required field'),
-    address: z.string().min(4, 'Required field'),
-    postCode: z.string().min(5, 'The post code must contain 5 characters'),
-    city: z.string().min(1, 'Required field'),
-    country: z.string().min(1, 'Required field'),
-    password: passwordSchema,
-    confirmPassword: passwordSchema,
-    phoneNumber: phoneSchema,
-    nip: nipSchema,
-    terms: termsSchema,
-})
+// export const RegistrationFormSchema = z.object({
+//     name: z.string().min(3, 'Required field'),
+//     lastName: z.string().min(3, 'Required field'),
+//     email: z.string().email('Please, type valid email'),
+//     companyName: z.string().min(1, 'Required field'),
+//     address: z.string().min(4, 'Required field'),
+//     postCode: z.string().min(5, 'The post code must contain 5 characters'),
+//     city: z.string().min(1, 'Required field'),
+//     country: z.string().min(1, 'Required field'),
+//     password: passwordSchema,
+//     confirmPassword: passwordSchema,
+//     phoneNumber: phoneSchema,
+//     nip: nipSchema,
+//     terms: termsSchema,
+// })
 
-export type RegistrationFormType = z.infer<typeof RegistrationFormSchema>;
+// export type RegistrationFormType = z.infer<typeof RegistrationFormSchema>;
 export type RegistrationFormShippingType = z.infer<typeof RegistrationFormShippingSchema>;
 
 // export const RegistrationFormSchema = (isLoggedIn: boolean, isCheckout: boolean = false, isShipping: boolean = false) =>
