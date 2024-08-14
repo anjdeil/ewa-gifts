d37c3ee7f6329b94790e1ff67de5e898f82e2799
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-08-14 17:59:46 +0200

    first commit

--- src/components/Blog/BlogList/BlogList.tsx
+++ src/components/Blog/BlogList/BlogList.tsx
@@ -5,31 +5,32 @@ import { BlogItemType, BlogListProps } from "@/types";
 import { useFetchAllBlogPostsQuery } from "@/store/wordpress";
 import { transformBlogCard } from "@/services/transformers";
 
-export const BlogList: FC<BlogListProps> = ({ data }) =>
-{
-    const { data: fetchedBlogPosts, isError } = useFetchAllBlogPostsQuery(undefined, {
-        skip: !!data,
-    });
+export const BlogList: FC<BlogListProps> = ({ data }) => {
+  // const { data: fetchedBlogPosts, isError } = useFetchAllBlogPostsQuery(undefined, {
+  //     skip: !!data,
+  // });
 
-    const blogPosts = useMemo(() =>
-    {
-        const posts = data || fetchedBlogPosts || [];
-        return transformBlogCard(posts);
+  // const blogPosts = useMemo(() =>
+  // {
+  //     const posts = data || fetchedBlogPosts || [];
+  //     return transformBlogCard(posts);
 
-    }, [data, fetchedBlogPosts]);
+  // }, [data, fetchedBlogPosts]);
 
-    if (isError)
-    {
-        return <h3>Blog posts not found.</h3>;
-    }
+  // if (isError)
+  // {
+  //     return <h3>Blog posts not found.</h3>;
+  // }
 
-    return (
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-            {blogPosts.map((post: BlogItemType, index: number) => (
-                <Grid item xs={12} sm={6} key={index} style={{ marginBottom: '20px' }}>
-                    <BlogListItem post={post} />
-                </Grid>
-            ))}
+  console.log("data...", data);
+
+  return (
+    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+      {data?.map((post: BlogItemType, index: number) => (
+        <Grid item xs={12} sm={6} key={index} style={{ marginBottom: "20px" }}>
+          <BlogListItem post={post} />
         </Grid>
-    );
+      ))}
+    </Grid>
+  );
 };
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,11 +1,31 @@
 // import wpRestApi from "@/services/wordpress/WPRestAPI";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-nocheck
+import { BlogList } from "@/components/Blog/BlogList";
+import { PageHeader } from "@/components/Layouts/PageHeader";
+import { Section } from "@/components/Layouts/Section";
+import { customRestApi } from "@/services/CustomRestApi";
+import { BlogItemSchema, BlogItemType } from "@/types";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
+import { FC } from "react";
+import { z } from "zod";
 
-const Blog = ({ response }) => {
+const BlogPropsSchema = z.object({
+  response: z.array(BlogItemSchema),
+  error: z.string().optional(),
+});
+
+type BlogProps = z.infer<typeof BlogPropsSchema>;
+
+const Blog: FC<BlogProps> = ({ response, error }) => {
   const pageTitle = "Blog";
   console.log(response);
+
+  if (error) {
+    throw new Error(error);
+  }
+
+  const breadLinks = [{ name: pageTitle, url: "" }];
 
   return (
     <>
@@ -14,15 +34,53 @@ const Blog = ({ response }) => {
         <meta name="description" content={`This is ${pageTitle}`} />
       </Head>
       <main>
-        <div className="container">
-          <h1>{pageTitle}</h1>
-          <p>Welcome to the Blog Page</p>
-        </div>
+        <Section className={"container"}>
+          <PageHeader title={pageTitle} breadLinks={breadLinks} />
+          <BlogList data={response} />
+        </Section>
       </main>
     </>
   );
 };
 
-export default Blog;
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  let response: BlogItemType[] | null = null;
+  let error: string | null = null;
 
-// response = await wpRestApi.get('posts');
+  try {
+    const allPostsResponse = await customRestApi.get(`posts`);
+
+    if (allPostsResponse) {
+      if (allPostsResponse.data) {
+        const allPosts = (
+          allPostsResponse.data as { data: { items: BlogItemType[] } }
+        ).data.items;
+
+        if (!allPosts.length) {
+          return { notFound: true };
+        }
+
+        response = allPosts;
+      } else {
+        throw new Error("There are no articles");
+      }
+    } else {
+      throw new Error("Server Error.");
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      error = err.message;
+    } else {
+      error = "Server Error.";
+    }
+  }
+
+  return {
+    props: {
+      response: response ?? null,
+      error,
+    },
+  };
+};
+
+export default Blog;
