2c5a6594e31443a2adaf706d40714a07dfe6a08f
Ilya-Zab <168741673+IliaDigiWay@users.noreply.github.com>
2024-08-20 15:07:11 +0200

    Uncommented top bar menu

--- src/components/Layouts/TopBar/TopBar.tsx
+++ src/components/Layouts/TopBar/TopBar.tsx
@@ -1,17 +1,18 @@
 import Link from "next/link";
 import Image from "next/image";
-// import Nav from "@/components/Layouts/Navigation/Nav";
+import Nav from "@/components/Layouts/Navigation/Nav";
 import React from 'react';
 import Socials from '../Socials/Socials';
 import styles from './styles.module.scss';
-const TopBar: React.FC = () => {
+const TopBar: React.FC = () =>
+{
     return (
         <div className={styles.topBar}>
             <div className={`${styles.topBar__stack} container`}>
                 <Link href={'/'} passHref className={styles.topBar__logoLink}>
                     <Image src="/logo.svg" className={styles.topBar__logoLinkImage} alt="Logo" width={150} height={40} />
                 </Link>
-                {/* <Nav
+                <Nav
                     menuId={816}
                     className={styles.topBar__nav}
                     skeleton={
@@ -22,7 +23,7 @@ const TopBar: React.FC = () => {
                             gap: '10px'
                         }
                     }
-                /> */}
+                />
                 <Socials
                     menuId={358}
                     skeleton={
