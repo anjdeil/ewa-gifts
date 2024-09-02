291fd148951ae19ea5b40444e23fe60c875fcba7
DigiwaymediaDenys <denys@digiwaymedia.com>
2024-09-02 17:58:44 +0200

    comment unnecessary transformers

--- src/services/transformers/index.ts
+++ src/services/transformers/index.ts
@@ -1,4 +1,4 @@
+export * from './pageBuilder';
 export * from './woocommerce/transformSearchBarCategories';
 export * from './woocommerce/transformSearchBarProducts';
-export * from './pageBuilder';
-export * from './wordpress';
+// export * from './wordpress';
--- src/services/transformers/wordpress/transfromBlogCard.ts
+++ src/services/transformers/wordpress/transfromBlogCard.ts
@@ -1,22 +1,22 @@
-import { BlogFetchData, BlogItemType } from "@/types/Blog";
+// import { BlogFetchData, BlogItemType } from "@/types/Blog";
 
-export const transformBlogCard = (cards: BlogFetchData): BlogItemType[] =>
-{
-    return (
-        cards.map(card => (
-            {
-                author: card.author,
-                id: card.id,
-                content: card.content.rendered,
-                excerpt: card.excerpt.rendered,
-                date: card.date,
-                image_src: card.image_src,
-                link: card.link,
-                slug: card.slug,
-                title: card.title.rendered,
-                categories: card.categories,
-                tags: card.tags,
-            }
-        ))
-    )
-}
+// export const transformBlogCard = (cards: BlogFetchData): BlogItemType[] =>
+// {
+//     return (
+//         cards.map(card => (
+//             {
+//                 author: card.author,
+//                 id: card.id,
+//                 content: card.content.rendered,
+//                 excerpt: card.excerpt.rendered,
+//                 date: card.date,
+//                 image_src: card.image_src,
+//                 link: card.link,
+//                 slug: card.slug,
+//                 title: card.title.rendered,
+//                 categories: card.categories,
+//                 tags: card.tags,
+//             }
+//         ))
+//     )
+// }
