5615ca3b1141df654b0a0df9c9179818fdfb6a79
camelCaserJs <evgen@digiwaymedia.com>
2024-09-03 23:21:54 +0200

    desired products finished conflicts

--- src/components/Shop/ProductCardsList/ProductCardsList.tsx
+++ src/components/Shop/ProductCardsList/ProductCardsList.tsx
@@ -14,9 +14,6 @@ export const ProductCardList: FC<ProductCardListProps> = ({ isLoading = false, i
     const isMobile = useMediaQuery('(max-width: 768px)');
     const isTablet = useMediaQuery('(max-width: 1024px)');
 
-    if (isLoading) return <ProductCardListSkeleton />;
-    if (isError) return <p>We cannot get the products</p>;
-
     const column = (isMobile && (columns?.mobile !== undefined ? columns.mobile : 2)) ||
         (isTablet && (columns?.tablet !== undefined ? columns.tablet : 4)) ||
         (columns?.desktop !== undefined ? columns.desktop : 4);
@@ -73,22 +70,26 @@ export const ProductCardList: FC<ProductCardListProps> = ({ isLoading = false, i
     }
 
     return (
-        <Box
-            className={`list-reset ${styles.productList} ${isShopPage && styles['productList_shop-page']}`}
-            style={{
-                gridTemplateColumns: `repeat(${column}, 1fr)`,
-            }}
-        >
-            {products && products.map((product, i) => (
-                <ProductCard
-                    key={`${product.id}-${i}`}
-                    product={product}
-                    desirable
-                    onDesire={handleDisire}
-                    wishlist={userData?.meta?.wishlist}
-                    wishlistLoading={userDataLoading}
-                />
-            ))}
-        </Box>
+        isLoading ?
+            <ProductCardListSkeleton /> :
+            isError ?
+                <p>We cannot get the products</p> :
+                <Box
+                    className={`list-reset ${styles.productList} ${isShopPage && styles['productList_shop-page']}`}
+                    style={{
+                        gridTemplateColumns: `repeat(${column}, 1fr)`,
+                    }}
+                >
+                    {products && products.map((product, i) => (
+                        <ProductCard
+                            key={`${product.id}-${i}`}
+                            product={product}
+                            desirable
+                            onDesire={handleDisire}
+                            wishlist={userData?.meta?.wishlist}
+                            wishlistLoading={userDataLoading}
+                        />
+                    ))}
+                </Box>
     );
 }
