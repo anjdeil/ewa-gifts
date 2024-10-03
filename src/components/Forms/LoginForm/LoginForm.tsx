df2ee050f04a21a60f99fef8bbf3d7473c2c7f69
IliaDigiWay <168741673+IliaDigiWay@users.noreply.github.com>
2024-10-03 09:52:18 +0200

    Update LoginForm.tsx

--- src/components/Forms/LoginForm/LoginForm.tsx
+++ src/components/Forms/LoginForm/LoginForm.tsx
@@ -43,7 +43,7 @@ export const LoginForm: FC = () =>
             {
                 const userToken = response.token;
                 const options = data.rememberMe
-                    ? { path: '/', expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 8), sameSite: 'none', secure: false }
+                    ? { path: '/', expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 8), sameSite: 'none', secure: true }
                     : { path: '/', sameSite: 'lax', secure: true };
                 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
                 // @ts-expect-error
