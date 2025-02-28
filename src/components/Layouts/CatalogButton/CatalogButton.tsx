0c6025636f145678581247188ae6296742ff5029
DigiwaymediaDenys <denys@digiwaymedia.com>
2025-02-28 13:53:38 +0100

    change word Katalog to Kategorie

--- src/components/Layouts/CatalogButton/CatalogButton.tsx
+++ src/components/Layouts/CatalogButton/CatalogButton.tsx
@@ -1,6 +1,6 @@
-import React, { FC } from "react";
-import styles from "./styles.module.scss"
 import Image from "next/image";
+import { FC } from "react";
+import styles from "./styles.module.scss";
 
 interface CatalogButtonPropsType {
     onClick: () => void
@@ -10,7 +10,7 @@ const CatalogButton: FC<CatalogButtonPropsType> = ({ onClick }) => {
     return (
         <button onClick={onClick} className={styles["catalog-button"]}>
             <Image className={styles["catalog-button__icon"]} src={'/images/catalog-button.svg'} width={20} height={20} alt="Menu" />
-            <span className={styles["catalog-button__text"]}>Katalog</span>
+            <span className={styles["catalog-button__text"]}>Kategorie</span>
         </button>
     )
 }
